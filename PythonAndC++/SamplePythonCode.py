6db7775ae0911a0965c9c9425654367069c19837
grmcrkrs <grmcrkrs@gmail.com>
2021-04-04 22:22:55 -0500

    Update SamplePythonCode.py

--- PythonAndC++/SamplePythonCode.py
+++ PythonAndC++/SamplePythonCode.py
@@ -1,14 +1,39 @@
+#in order fo this to work, you must save any changes to the file
+#in the Python executable folder. Very important or will give 
+#write access violation error 
+#-for GrmCrkrs system only
+
 import re
 import string
 
 
 def printsomething():
-    print("Hello from python!")
+    print("GrmCrkrs is a Beast!")
 
 def PrintMe(v):
     print("You sent me: " + v)
     return 100;
 
+#in order fo this to work, you must save any changes to the file
+#in the Python executable folder. Very important or will give 
+#write access violation error
+def DoubleValue(v): 
+    print(v + v)
+
 def SquareValue(v):
-    return v * v
+    print(v + v)
+
+def MultiplicationTable(v):
+    print(str(v) + " X 1 = " + str(v))
+    print(str(v) + " X 2 = " + str(v*2))
+    print(str(v) + " X 3 = " + str(v*3))
+    print(str(v) + " X 4 = " + str(v*7))
+    print(str(v) + " X 5 = " + str(v*5))
+    print(str(v) + " X 6 = " + str(v*6))
+    print(str(v) + " X 7 = " + str(v*7))
+    print(str(v) + " X 8 = " + str(v*8))
+    print(str(v) + " X 9 = " + str(v*9))
+    print(str(v) + " X 10 = " + str(v*10))
+
+
 
